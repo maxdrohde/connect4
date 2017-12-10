be82608e15b721ebe100d770a6643a8c4cce1c3a
calebbraun <calebjbraun@gmail.com>
2017-12-10 14:18:20 -0500

    Put gameplay code in new class

--- play_game.py
+++ play_game.py
@@ -1,33 +1,40 @@
 from Board import Board
-import os
-board = Board()
+import os, sys
 
-turn = 1
+class Game:
+    def __init__(self, bot1 = None, bot2 = None):
+        self.game_board = Board()
+        self.current_turn = 1
 
-def flip(x):
-    if x == 1:
-        return (2)
-    else:
-        return(1)
+    def play(self):
+        while self.game_board.check_win() == False:
+            os.system('clear')
+            self.game_board.print_board()
+            num = self.get_column()
+            success = self.game_board.drop(num,self.current_turn)
+            if success == True:
+                if self.current_turn == 1:
+                    self.current_turn = 2
+                else:
+                    self.current_turn = 1
 
-def get_column():
-    number = input('Column?     ')
-    while not number.isdigit() or int(number) not in range(1,8):
-        number = input('Enter a proper column number!')
+        os.system('clear')
+        self.game_board.print_board()
+        print('Player '+str(self.flip(self.current_turn))+' wins!')
 
-    return(int(number))
-
-while board.check_win() == False:
-    os.system('clear')
-    board.print_board()
-    num = get_column()
-    success = board.drop(num,turn)
-    if success == True:
-        if turn == 1:
-            turn = 2
+    def flip(self, x):
+        if x == 1:
+            return (2)
         else:
-            turn = 1
+            return(1)
 
-os.system('clear')
-board.print_board()
-print('Player '+str(flip(turn))+' wins!')
+    def get_column(self):
+        number = input('Column?     ')
+        while not number.isdigit() or int(number) not in range(1,8):
+            number = input('Enter a proper column number!')
+
+        return(int(number))
+
+if __name__ == '__main__':
+    game = Game()
+    game.play()
