f560bf6985d306be12dfc2f5ae3b594b8ccecf02
calebbraun <calebjbraun@gmail.com>
2017-12-10 01:53:03 -0500

    Make board prettier

--- Board.py
+++ Board.py
@@ -1,9 +1,18 @@
+from helpers import colors, shapes
+
 class Board:
 
     def __init__(self):
         self.height = 6
         self.width = 7
-        self.board = [['0' for x in range(self.width)] for x in range(self.height)]
+
+        self.blank = colors.WHITE + shapes.CIRCLE + colors.ENDC
+        self.p1 = colors.CYAN + shapes.CIRCLE + colors.ENDC
+        self.p2 = colors.RED + shapes.CIRCLE + colors.ENDC
+
+        row = [self.blank for x in range(self.width)]
+        self.board = [row[:] for x in range(self.height)]
+
 
     def drop(self,column_number,player_number):
         column_number = column_number - 1
@@ -12,20 +21,20 @@ class Board:
             column.append(row[column_number])
 
         current_row = len(column)-1
-        while (column[current_row]!='0'):
+        while (column[current_row]!=self.blank):
             current_row = current_row -1
             if current_row == -1:
                 print('\007')
                 return(False)
-        self.board[current_row][column_number] = str(player_number)
+        piece = self.p1 if player_number == 1 else self.p2
+        self.board[current_row][column_number] = piece
         return(True)
-
 
 
     def check_row(self,array):
         for row in array:
             row_string = ''.join(row)
-            if '1'*4 in row_string or '2'*4 in row_string:
+            if self.p1*4 in row_string or self.p2*4 in row_string:
                 return(True)
         return(False)
 
@@ -33,8 +42,15 @@ class Board:
         return ([list(x) for x in zip(*array)])
 
     def print_board(self):
+        column_numbers = '  ' + ' '.join([str(i+1) for i in range(self.width)])
+        print(column_numbers)
+        print(shapes.BAR_TOP_LEFT + shapes.BAR_H * self.width * 2 + shapes.BAR_TOP_RIGHT)
+
         for row in self.board:
-            print(row)
+            row_str = ' '.join(row)
+            print(shapes.BAR_V + row_str + " " + shapes.BAR_V)
+
+        print(shapes.BAR_BOTTOM_LEFT + shapes.BAR_H * self.width * 2 + shapes.BAR_BOTTOM_RIGHT)
 
     def check_win(self):
         if self.check_row(self.board) or self.check_row(self.transpose(self.board)):
---
+++ helpers.py
@@ -0,0 +1,22 @@
+class colors:
+    BLACK   = "\033[0;30m"
+    RED     = "\033[1;31m"
+    GREEN   = "\033[0;32m"
+    YELLOW  = "\033[0;33m"
+    BLUE    = "\033[0;34m"
+    MAGENTA = "\033[0;35m"
+    CYAN    = "\033[0;36m"
+    WHITE   = "\033[0;37m"
+    ENDC    = "\033[0;0m"
+
+class shapes:
+    BAR_V = u"\u2551"
+    BAR_H = u"\u2550"
+    BAR_TOP_LEFT = u"\u2554"
+    BAR_TOP_RIGHT = u"\u2557"
+    BAR_BOTTOM_LEFT = u"\u255A"
+    BAR_BOTTOM_RIGHT = u"\u255D"
+
+    CIRCLE = u"\u2B24"
+    HOLLOW_CIRCLE = u"\u25CB"
+    LARGE_CIRCLE = u"\u25EF"
