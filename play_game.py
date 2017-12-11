da16ca2ea1cda4c23ce777ac2071d760351a71ba
Maximilian Dimitrios Rohde <Max@max.local>
2017-12-10 20:45:51 -0500

    added time delay to bot vs bot games

--- play_game.py
+++ play_game.py
@@ -1,6 +1,7 @@
 from Board import Board
 from helpers import load_bot
 import os, sys
+import time
 
 class Game:
     def __init__(self, bot1 = None, bot2 = None):
@@ -34,8 +35,10 @@ class Game:
     def get_column(self):
         if self.current_turn == 1 and self.bot1 != None:
             number = self.bot1.play_piece(self.game_board)
+            time.sleep(0.5)
         elif self.current_turn == 2 and self.bot2 != None:
             number = self.bot1.play_piece(self.game_board)
+            time.sleep(0.5)
         else:
             number = input('Column?     ')
             while not number.isdigit() or int(number) not in range(1,8):
