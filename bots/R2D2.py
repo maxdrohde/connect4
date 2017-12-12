d3655a1a33079f86736464b6fc91bc0345a380da
calebbraun <calebjbraun@gmail.com>
2017-12-11 23:14:19 -0500

    Fix off by one error in R2D2

--- bots/R2D2.py
+++ bots/R2D2.py
@@ -7,6 +7,9 @@ class R2D2:
 
     def play_piece(self, board):
         """ Always chooses a random column """
+
+        # All columns start out available. For example, this may start as a list
+        # [0, 1, 2, 3, 4, 5, 6]
         available_columns = [i for i in range(len(board.board[0]))]
         rand_col = self.get_random_column(available_columns)
 
@@ -16,7 +19,7 @@ class R2D2:
             rand_col = self.get_random_column(available_columns)
 
         # The board columns are numbered for humans, so increase by one
-        return rand_col
+        return rand_col + 1
 
     def get_random_column(self, available_columns):
         rand_num = randint(0, len(available_columns) - 1)
